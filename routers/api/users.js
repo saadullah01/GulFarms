f5786fcd4c2700ca785be22ace7d9bb8c40bdc2d
arhamgit <21100130@lums.edu.pk>
2020-04-24 16:34:43 +0500

    arham trying

--- routers/api/users.js
+++ routers/api/users.js
@@ -9,7 +9,7 @@ const validateRegisterInput = require("../../validation/register");
 const validateLoginInput = require("../../validation/login");
 
 // User Model
-const User = require('../../models/User');
+// const User = require('../../models/User');
 
 // // @route   GET api/users
 // // @desc    Get All Users
