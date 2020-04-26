b7e133f0aed7c11e9a4b29a4b0f2077699c39c25
arhamGIT <59977509+arhamGIT@users.noreply.github.com>
2020-04-26 18:04:17 +0500

    heroku try 1000

--- app.js
+++ app.js
@@ -42,9 +42,14 @@ app.use('/api/users', users);
 
 if(process.env.NODE_ENV === 'production'){
     // app.use(express.static(path.resolve(__dirname,'client','build','index.html')));
-    app.use('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-    });
+    // app.use('*',(req,res)=>{
+    //     res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    // });
+    const root = path.join(__dirname, 'client', 'build')
+    app.use(express.static(root));
+    app.get("*", (req, res) => {
+    res.sendFile('index.html', { root });
+    })
 }
 
 // PORT (Starting Server)
