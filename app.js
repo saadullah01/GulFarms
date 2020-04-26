6778e1575ed43d64df75ce9c9ae8276e7ab8147a
arhamGIT <59977509+arhamGIT@users.noreply.github.com>
2020-04-26 17:38:07 +0500

    heroku try 9

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.use(bodyParser.json());
 
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(__dirname + '/client/build'));
+    app.use(express.static(path.resolve(__dirname,'client','build','index.html')));
     // app.use('*',(req,res)=>{
     //     res.sendFile(path.resolve(__dirname,'../client/build','index.html'));
     // });
