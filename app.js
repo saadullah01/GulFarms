07ef6f5edde40e85721a735784b708d45fdea9a5
arhamGIT <59977509+arhamGIT@users.noreply.github.com>
2020-04-26 17:00:56 +0500

    Revert "fixing heroku stuff"
    This reverts commit d4e20e56a930a471ca25dba57f1007b7f62bb1fb.

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(bodyParser.json());
 //in production
 if(process.env.NODE_ENV === 'production'){
     app.use('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client\build\index.html'));
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
     });
 }
 
