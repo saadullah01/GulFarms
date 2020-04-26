3d4689cc9c98e9f195687def4439cdbfa819abc2
arhamGIT <59977509+arhamGIT@users.noreply.github.com>
2020-04-26 16:29:47 +0500

    heroku tr2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 // const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 // const passport = require("passport");
-
+const path =require('path');
 // Routes
 const users = require('./routers/api/users');
 
@@ -33,6 +33,13 @@ app.use(bodyParser.json());
 
 // Use Routes
 // app.use('./api/users', users);
+
+//in production
+if(process.env.NODE_ENV === 'production'){
+    app.use('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    });
+}
 
 // PORT (Starting Server)
 const port = process.env.PORT || 5000;
