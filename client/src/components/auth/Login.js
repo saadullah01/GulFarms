87df6690fe832858b00d6bdabd93e8090ca2597d
arhamGIT <59977509+arhamGIT@users.noreply.github.com>
2020-04-27 01:32:12 +0500

    login working

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,9 +1,13 @@
-import React, { Component } from 'react';
-import { Link } from "react-router-dom";
-import { 
-    Button, 
-    Form, 
-    FormGroup, 
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { loginUser } from "../../actions/authActions";
+import classnames from "classnames";
+import {
+    Button,
+    Form,
+    FormGroup,
     Input
 } from 'reactstrap';
 
@@ -11,50 +15,71 @@ class Login extends Component {
     state = {
         email: "",
         password: "",
-        errors: {}        
+        errors: {}
     }
 
     onChange = e => {
         this.setState({ [e.target.id]: e.target.value });
     }
+    componentWillReceiveProps(nextProps) {
 
+        if (nextProps.loggedIn) {
+            this.props.history.push("/home");
+        }
+    }
     onSubmit = e => {
         e.preventDefault();
         const userData = {
             email: this.state.email,
             password: this.state.password
         }
-        console.log(userData);
+        this.props.loginUser(userData);
+
     }
 
-    render() {
-        const { errors } = this.state;
+    render(props) {
+        const { errors } = this.props;
+
         return (
             <div className="home-page">
                 <div className="container main">
                     <p className="brand-name">GUL FARMS</p>
                     <Form className="reg-form" noValidate onSubmit={this.onSubmit}>
                         <FormGroup>
-                            <Input 
+                            <Input
                                 className="input-field"
-                                type="email" 
+                                type="email"
                                 placeholder="Enter your email address"
                                 onChange={this.onChange}
-                                value={this.state.email} 
-                                error={errors.email} 
+                                value={this.state.email}
+                                error={errors.email}
                                 id="email"
+                                className={classnames("", {
+                                    invalid: errors.email || errors.emailnotfound
+                                })}
                             />
                         </FormGroup>
+                        <span className="red-text">
+                            {errors.email}
+                            {errors.emailnotfound}
+                        </span>
                         <FormGroup className="password-container">
-                            <Input 
+                            <Input
                                 className="input-field"
-                                type="password" 
-                                placeholder="Enter your password" 
+                                type="password"
+                                placeholder="Enter your password"
                                 onChange={this.onChange}
-                                value={this.state.password} 
-                                error={errors.password} 
+                                value={this.state.password}
+                                error={errors.password}
                                 id="password"
+                                className={classnames("", {
+                                    invalid: errors.password || errors.passwordincorrect
+                                })}
                             />
+                            <span className="red-text">
+                                {errors.password}
+                                {errors.passwordincorrect}
+                            </span>
                             {/* <div className="pop-up">
                                 Password must be greater than 8 characters long and
                                 must contain atleast 1 digit and 1 special character
@@ -65,10 +90,17 @@ class Login extends Component {
                             <Button className="login-btn">LOGIN</Button>
                         </div>
                     </Form>
-                </div>    
+                </div>
             </div>
         )
     }
 }
 
-export default Login;
+const mapStateToProps = state => ({
+    loggedIn: state.authReducer.islogged,
+    errors: state.errorReducer.errors
+});
+export default connect(
+    mapStateToProps,
+    { loginUser }
+)(withRouter(Login));
