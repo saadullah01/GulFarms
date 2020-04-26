c11144ad9d3361a76e5a416f0f49105cf38b5b69
m-jojo-s <21100025@lums.edu.pk>
2020-04-26 17:52:34 +0500

    created models/farm.js and baseModels.js

--- models/User.js
+++ models/User.js
@@ -3,22 +3,10 @@ const Schema = mongoose.Schema;
 
 // Create Schema
 const UserSchema = new Schema({
-    fname: {
-        type: String,
-        required: true
-    },
-    lname: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+    fname: {type: String, required: true},
+    lname: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true}
 }, {timestamps: true});
 
 module.exports = User = mongoose.model('user', UserSchema);
