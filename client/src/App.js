d68f95d5d6955e9f5e217df1e103f1f7a1d0f634
arhamGIT <59977509+arhamGIT@users.noreply.github.com>
2020-04-27 18:19:07 +0500

    still testing

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,6 @@ import ResetPassword from './components/auth/ResetPassword';
 import FarmsList from './components/FarmsList';
 import Farm from "./components/Farm"
 import Landing from './components/Landing';
-import Farm from './components/Farm';
 import CreateFarm from './components/CreateFarm';
 
 import jwt_decode from "jwt-decode";
