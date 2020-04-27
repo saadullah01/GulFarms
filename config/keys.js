068ad8f52e69fe89f80ba6d55280b60793287203
m-jojo-s <21100025@lums.edu.pk>
2020-04-28 01:32:20 +0500

    Integrated Login and Password Recovery systems

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
     mongoURI: 'mongodb+srv://saad:gulfarms@gulfarms-s9ioc.mongodb.net/test?retryWrites=true&w=majority',
-    sendgridEMAIL: '21100025@lums.edu.pk',
+    sendgridEMAIL: 'm.arham4000@gmai.com',
     secretOrKey: "secret"
 };
